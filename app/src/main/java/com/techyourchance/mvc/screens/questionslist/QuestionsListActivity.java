983649513fb7c7c21519a15f1bcbe0a82e18cb8c
StephenGemin <45926479+StephenGemin@users.noreply.github.com>
2019-09-24 19:42:47 -0400

    Refactoring QuestionsList screen to use RecyclerView

--- app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListActivity.java
+++ app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListActivity.java
@@ -1,14 +1,7 @@
 package com.techyourchance.mvc.screens.questionslist;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.techyourchance.mvc.R;
--- app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListViewMvcImpl.java
+++ app/src/main/java/com/techyourchance/mvc/screens/questionslist/QuestionsListViewMvcImpl.java
@@ -2,10 +2,11 @@ package com.techyourchance.mvc.screens.questionslist;
 
 import android.content.Context;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 
 import com.techyourchance.mvc.R;
 import com.techyourchance.mvc.questions.Question;
@@ -16,19 +17,20 @@ import java.util.List;
 /**
  * Created by Stephen Gemin on 9/23/2019
  */
-public class QuestionsListViewMvcImpl implements QuestionsListAdapter.OnQuestionClickListener,
+public class QuestionsListViewMvcImpl implements QuestionsRecyclerAdapter.Listener,
         QuestionListViewMvc  {
 
-    private ListView mLstQuestions;
-    private QuestionsListAdapter mQuestionsListAdapter;
+    private RecyclerView mRecyclerQuestions;
+    private QuestionsRecyclerAdapter mAdapter;
     private final View mRootView;
     private final List<Listener> mListeners = new ArrayList<>(1);
 
     public QuestionsListViewMvcImpl(LayoutInflater inflater, @Nullable ViewGroup parent) {
         mRootView = inflater.inflate(R.layout.layout_questions_list, parent, false);
-        mLstQuestions = findViewById(R.id.lst_questions);
-        mQuestionsListAdapter = new QuestionsListAdapter(getContext(),this);
-        mLstQuestions.setAdapter(mQuestionsListAdapter);
+        mRecyclerQuestions = findViewById(R.id.recycler_questions);
+        mRecyclerQuestions.setLayoutManager(new LinearLayoutManager(getContext()));
+        mAdapter = new QuestionsRecyclerAdapter(inflater,this);
+        mRecyclerQuestions.setAdapter(mAdapter);
     }
 
     @Override
@@ -48,9 +50,7 @@ public class QuestionsListViewMvcImpl implements QuestionsListAdapter.OnQuestion
 
     @Override
     public void bindQuestions(List<Question> questions) {
-        mQuestionsListAdapter.clear();
-        mQuestionsListAdapter.addAll(questions);
-        mQuestionsListAdapter.notifyDataSetChanged();
+        mAdapter.bindQuestions(questions);
     }
 
     @Override
